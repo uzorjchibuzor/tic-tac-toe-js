3d6d5abb68a195e588af64f227291c02c83ca71d
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2019-01-07 20:11:02 +0100

    game is ending correctly with winner and draw

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const Player = (name, shape) => {
   const selectedCells = [];
-  const setName = newName => name = newName;
+  const setName = newName => (name = newName);
   return { name, shape, selectedCells };
-}
+};
 
 class Gameboard {
   constructor() {
@@ -10,55 +10,65 @@ class Gameboard {
     this.player2 = Player("Player 2", "O");
     this.currentPlayer = this.player1;
     this.board = ["", "", "", "", "", "", "", "", ""];
-    this.gameOver = false;
+    this.gameOver = false
   }
 
   swapTurn() {
     if (this.currentPlayer == this.player1) {
-      return this.currentPlayer = this.player2
-    } else return this.currentPlayer = this.player1;
+      return (this.currentPlayer = this.player2);
+    } else return (this.currentPlayer = this.player1);
   }
 
   playerSelection(shape, index) {
     if (!this.board[index]) {
       this.board[index] = shape;
-      board.children[index].innerHTML = shape
+      board.children[index].innerHTML = shape;
       this.currentPlayer.selectedCells.push(index);
-      if (this.endGame()) {
-        alert(`${this.currentPlayer.name} Wins`);
-        window.location.reload()
+      if (this.endGame() == this.currentPlayer.name) {
+        alert(`${this.currentPlayer.name} Wins`)
+        this.startNewGame()
+      } else if (this.endGame() == 'draw') {
+        alert('The game is a draw')
+        this.startNewGame();
       }
+      // console.log(this.endGame());
       // this.drawGame();
-      this.swapTurn();    
+      this.swapTurn();
     } else {
       alert("Select an empty cell");
-    }   
-    
+    }
   }
 
-
+  startNewGame() {
+    return window.location.reload();
+  }
 
   endGame() {
-    const winningCondition =[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
- 
+    const winningCondition = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
     for (let combination of winningCondition) {
       let count = 0;
-      for (let i = 0; i < combination.length; i++) {
-        if (this.currentPlayer.selectedCells.includes(combination[i])) {
+      for (let value of combination) {
+        if (this.currentPlayer.selectedCells.includes(value)) {
           count++;
         }
-        if (count == 3) {
-        return true
-        }
       }
+      if (count == 3) {
+        return this.gameOver = this.currentPlayer.name
+      } else if (this.player1.selectedCells.length + this.player2.selectedCells.length == 9) {
+        return this.gameOver = 'draw'
+      } 
     }
-
-    if (this.player1.selectedCells.length + this.player2.selectedCells.lenght == 9) {
-      return true
-    }
-    return false
-  } 
-  
+  }
 }
 
 const game = new Gameboard();
@@ -71,19 +81,16 @@ game.board.forEach((cell, index) => {
   let div = document.createElement("div");
   div.innerHTML = cell;
   div.id = index;
-  div.classList = 'col-md-4';
+  div.classList = "col-md-4";
   board.appendChild(div);
-})
+});
 
-let instructions = document.querySelector('.instructions');
+let instructions = document.querySelector(".instructions");
 
-
-
-let cells = document.querySelectorAll('.col-md-4');
+let cells = document.querySelectorAll(".col-md-4");
 
 cells.forEach((cell, index) => {
-  cell.addEventListener('click', () => game.playerSelection(game.currentPlayer.shape, index));
-})
-
-
-  
+  cell.addEventListener("click", () =>
+    game.playerSelection(game.currentPlayer.shape, index)
+  );
+});
